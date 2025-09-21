94fd7850826e472a79cb95a7f078814d30d62750
doheon <heizence6626@gmail.com>
2025-09-21 15:41:06 +0900

    add : Define Exception class, Error codes enum types, Exception handler

---
+++ part01-todo-api/backend/src/main/java/com/todoapi/backend/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.todoapi.backend.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    // Common
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "Invalid Input Value"),
+
+    // Todo
+    TODO_NOT_FOUND(HttpStatus.NOT_FOUND, "Todo not found");
+
+    private final HttpStatus status;
+    private final String message;
+}
