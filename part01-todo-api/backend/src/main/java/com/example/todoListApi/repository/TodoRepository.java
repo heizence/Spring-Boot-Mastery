97a7a9256639e877edf9de76899f4fc0777d6220
doheon <heizence6626@gmail.com>
2025-10-08 18:11:28 +0900

    Edit : Edit root directory name

--- part01-todo-api/backend/src/main/java/com/example/todoListApi/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.todoListApi.repository;
-
-import com.example.todoListApi.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// By extending JpaRepository<Todo, Long>, we get methods like save(), findById(), findAll(), deleteById() for free.
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
